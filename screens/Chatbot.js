f6b2c12f726ec89f4a43ea5813a2e5062780fab5
evertonSouzaMeli <everton.vsilva@mercadolivre.com>
2022-09-30 14:22:15 -0300

    feat: text-to-speech support

--- screens/Chatbot.js
+++ screens/Chatbot.js
@@ -3,9 +3,10 @@ import {useEffect, useState} from 'react';
 import {Button, Text, View, StyleSheet, TextInput, AsyncStorage} from 'react-native';
 import Constants from 'expo-constants';
 import base64 from 'react-native-base64';
-import SpeechToText from '../components/SpeechToText';
 import axios from 'axios';
+import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
 import * as Speech from "expo-speech";
+
 
 export default function Chatbot() {
     const [texto, setTexto] = useState("")
@@ -14,10 +15,12 @@ export default function Chatbot() {
     const key = "PJzIsPSS51vZx8VoQLdwyuXXvOZZd52uOUBp1KWmyZTu"
     const encodedKey = base64.encode(`apikey:${key}`)
 
+    const { transcript, listening, resetTranscript, browserSupportsSpeechRecognition } = useSpeechRecognition();
+
     const enviarTextoParaChatbot = () => {
         axios.post(
             `https://api.us-south.assistant.watson.cloud.ibm.com/instances/11a13d07-78ef-4242-89cc-782d5bd13ceb/v1/workspaces/2f75e230-f055-4276-a75b-b2c2ae8ba53a/message?version=2018-09-20`,
-            {input: {text: texto}},
+            {input: {text: transcript}},
             {
                 headers: {
                     Authorization: `Basic ${encodedKey}`,
@@ -28,7 +31,7 @@ export default function Chatbot() {
             .then((res) => {
                 let resp = res.data.output.text
                 setResposta(resp)
-                Speech.speak(resp, { language: "pt-BR" } );(res.data.output.text)
+                Speech.speak(resp, { language: "pt-BR" } );
             })
             .catch((err) => {
                 alert(err)
@@ -36,14 +39,17 @@ export default function Chatbot() {
     }
 
     return (
-        <View style={{ display: 'flex', flexDirection: 'row' }}>
-            <View>
-                <TextInput style={{ margin: 5, backgroundColor: 'white', borderColor: 'blue', borderWidth: 3 }} placeholderTextColor={"Entre com texto"} value={texto}  onChangeText={ value => { setTexto(value) }} />
+        <View style={{ display: 'flex', alignItems: 'center' }}>
+            <View style={{ flexDirection: 'row'}}>
+                <TextInput style={{ margin: 5, backgroundColor: 'white', borderColor: 'blue', borderWidth: 3 }} value={transcript}/>
+                <TextInput style={{ margin: 5, backgroundColor: 'white', borderColor: 'green', borderWidth: 3 }} value={resposta}/>
+            </View>
+
+            <View style={{ margin: 5, flexDirection: 'row' }}>
+                <Button title={"Gravar"} onPress={ SpeechRecognition.startListening } />
                 <Button title={"Enviar"} onPress={ () => { enviarTextoParaChatbot() }} />
             </View>
-            <View>
-                <TextInput style={{ margin: 5, backgroundColor: 'white', borderColor: 'green', borderWidth: 3 }}  placeholderTextColor={"Entre com texto"}  value={resposta}/>
-            </View>
+            { listening ? <Text>Gravando...</Text> : undefined }
         </View>
     );
 }
